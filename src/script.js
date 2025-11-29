2fdcf539eb56f7ab1ef8747265653eb4e1375ba5
shrub <shrub719@gmail.com>
2025-11-29 12:36:31 +0000

    revert initial commit

--- src/script.js
+++
@@ -1,9 +0,0 @@
-currentSection = "about"
-
-function switchSection(section) {
-    document.getElementById(`l-${currentSection}`).classList.remove("active");
-    document.getElementById(`c-${currentSection}`).classList.remove("active");
-    currentSection = section;
-    document.getElementById(`l-${currentSection}`).classList.add("active");
-    document.getElementById(`c-${currentSection}`).classList.add("active");
-}
